c14ea6054d52fe227bd5cf0cbcea53d538a0d218
Regor24 <eradkin@aspirity.com>
2017-07-27 15:32:37 +0700

    Change structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,6 @@ var browserSync = require('browser-sync').create();
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var del = require('del');
 var runSequence = require('run-sequence');
 var plumber = require('gulp-plumber');
 var cssGlobbing = require('gulp-css-globbing');
@@ -15,78 +13,31 @@ var notify = require("gulp-notify");
 var babel = require('gulp-babel');
 
 gulp.task('style', function () {
-  return gulp.src('src/scss/style.scss')
+  return gulp.src('./scss/style.scss')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(sass())
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
-gulp.task('script', function() {
-  return gulp.src('src/js/src/script.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('src/js/'));
-});
+// Пока без бабеля
+// gulp.task('script', function() {
+//   return gulp.src('src/js/src/script.js')
+//     .pipe(babel({
+//       presets: ['es2015']
+//     }))
+//     .pipe(gulp.dest('src/js/'));
+// });
 
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: 'src'
+      baseDir: './'
     },
   })
-});
-
-gulp.task('cssmin', function(){
-  return gulp.src('src/css/**/*.css')
-    .pipe(cssnano())
-    .pipe(gulp.dest('./css'))
-});
-
-gulp.task('html', function(){
-  return gulp.src('src/*.html')
-    .pipe(gulp.dest('./'))
-});
-
-gulp.task('scripts', function() {
-  return gulp.src('src/js/**/*.js')
-    //.pipe(uglify())
-    .pipe(gulp.dest('./js'));
-});
-
-gulp.task('images', function(){
-  return gulp.src('src/images/**/*.+(png|jpg|gif|svg)')
-  // Caching images that ran through imagemin
-  .pipe(cache(imagemin({
-      interlaced: true
-    })))
-  .pipe(gulp.dest('./images'))
-});
-
-gulp.task('pictures', function(){
-  return gulp.src('src/pictures/**/*.+(png|jpg|gif|svg)')
-  // Caching images that ran through imagemin
-  .pipe(cache(imagemin({
-      interlaced: true
-    })))
-  .pipe(gulp.dest('./pictures'))
-});
-
-gulp.task('fonts', function() {
-  return gulp.src('src/fonts/**/*')
-  .pipe(gulp.dest('./fonts'))
-});
-
-gulp.task('clean:dist', function() {
-  return del.sync('./');
-});
-
-gulp.task('cache:clear', function (callback) {
-return cache.clearAll(callback)
 });
 
 /////////////////////////////////////////////////
@@ -94,17 +45,14 @@ return cache.clearAll(callback)
 
 gulp.task('watch', ['browserSync'], function() {
   // Watch .scss files
-  gulp.watch('src/scss/**/*.scss', ['style']);
+  gulp.watch('./scss/**/*.scss', ['style']);
   // Reloads the browser whenever HTML or JS files change
-  gulp.watch('src/*.html', browserSync.reload);
-  gulp.watch('src/js/**/*.js', browserSync.reload);
+  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('./js/**/*.js', browserSync.reload);
 });
 
 gulp.task('build', function (callback) {
-  runSequence('clean:dist',
-    ['style', 'html', 'cssmin', 'scripts', 'images', 'pictures', 'fonts'],
-    callback
-  )
+  runSequence(['style'], callback)
 });
 
 gulp.task('default', function(callback) {
